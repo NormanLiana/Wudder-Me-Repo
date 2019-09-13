e7330d71ba5b5fc8f6868fdbbf39f6632d2ec245
NormanLiana <liananorman@pursuit.org>
2019-09-13 15:14:47 -0400

    Initial Commit

---
+++ Wudder-Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wudder-Me
+//
+//  Created by Liana Norman on 9/13/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wudder-MeTests/Wudder_MeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Wudder_MeTests.swift
+//  Wudder-MeTests
+//
+//  Created by Liana Norman on 9/13/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import XCTest
+@testable import Wudder_Me
+
+class Wudder_MeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
