8fbf5aae11d3238b2d21b78384489b2654a92714
NormanLiana <liananorman@pursuit.org>
2019-09-16 14:29:42 -0400

    adds 4 cocoa touch files for the VC's

---
+++ Wudder-Me/Controllers/PlantSearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlantSearchViewController.swift
+//  Wudder-Me
+//
+//  Created by Liana Norman on 9/16/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class PlantSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ Wudder-Me/Controllers/UsersPlantListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UsersPlantListViewController.swift
+//  Wudder-Me
+//
+//  Created by Liana Norman on 9/16/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class UsersPlantListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
