092292a69e371e0f7c148a6c5cd6aca7241168b1
Bee <biancabrown@pursuit.org>
2019-09-17 16:40:31 -0400

    Added a Cocoa Touch file named PlantTableViewCell for our custom cell.

---
+++ Wudder-Me/Controllers/PlantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlantTableViewCell.swift
+//  Wudder-Me
+//
+//  Created by Bianca Brown on 9/17/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
